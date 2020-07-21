d60769cee144db29e3cc04cdb9f98bd4f0da5742
Tobi Jackson <jtsproduction@live.com>
2020-07-21 20:35:17 +0100

    COTD

---
+++ programs/cotdclass.py
@@ -0,0 +1,21 @@
+
+"""
+Define a class named Rectangle, which can be constructed by a length and width.
+The Rectangle class needs to have a method that can compute area.
+Finally, write a unit test to test this method.
+"""
+import random
+
+class rectangle():
+
+    def __int__(self, width, length):
+        self.width=width
+        self.length=length
+
+    def area():
+        return length*width
+
+
+length=random.randint(1,100)
+width=random.randint(1,100)
+
---
+++ tests/codtclass_test.py
@@ -0,0 +1,6 @@
+import pytest
+from programs import cotdclass
+
+def test_area():
+    for _ in range(20):
+        assert (cotdclass.rectangle.area)
